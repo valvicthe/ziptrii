ed7d564a98de31e991f8d1b1a39358c71478ac27
valvicthe <louie12brett@icloud.com>
2026-07-12 11:20:28 +0100

    Update page.js

--- src/app/groups/create/page.js
+++ src/app/groups/create/page.js
@@ -1,6 +1,8 @@
 import { query } from '@/lib/db';
 import { redirect } from 'next/navigation';
 
+export const dynamic = 'force-dynamic';
+
 export default async function CreateGroupPage() {
   async function createGroup(formData) {
     'use server';
