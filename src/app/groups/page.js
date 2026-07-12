25efbd3641e2dec5b167c7697bf5bbc45bfad633
valvicthe <louie12brett@icloud.com>
2026-07-12 11:20:15 +0100

    Update page.js

--- src/app/groups/page.js
+++ src/app/groups/page.js
@@ -1,6 +1,8 @@
 import { query } from '@/lib/db';
 import Link from 'next/link';
 
+export const dynamic = 'force-dynamic';
+
 export default async function GroupsPage() {
   const groups = (await query('SELECT * FROM groups')).rows;
 
